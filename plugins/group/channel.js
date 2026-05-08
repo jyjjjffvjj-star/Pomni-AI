711f0a44419d59f3545000830b4ff3779e5f6e27
jyjjjffvjj-star <jyjjjffvjj@gmail.com>
2026-05-08 18:05:58 +0300

    Update footer text in sendButton function

--- plugins/group/channel.js
+++ plugins/group/channel.js
@@ -34,7 +34,7 @@ const handler = async (m, { conn, text }) => {
     await conn.sendButton(m.chat, {
       imageUrl: img,
       bodyText: msg,
-      footerText: "VENi ~ VII7",
+      footerText: "ALHWARY",
       buttons: [
         {
           name: "cta_copy",
