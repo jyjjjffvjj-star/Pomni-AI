dc70294704359beb7d4184237cec2a2060d71776
jyjjjffvjj-star <jyjjjffvjj@gmail.com>
2026-05-14 09:39:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '2348090757706', // Bot number
+  phoneNumber: '79020027922', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
