4eebad5aeab779562173ca6f15305721de2656e0
jyjjjffvjj-star <jyjjjffvjj@gmail.com>
2026-05-19 12:57:39 +0300

    Update yt-button.js

--- plugins/download/yt-button.js
+++ plugins/download/yt-button.js
@@ -14,7 +14,7 @@ const handler = async (m, { conn, text }) => {
             { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
         ],
         mentions: [m.sender],
-        newsletter: { name: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", jid: "120363225356834044@newsletter" },
+        newsletter: { name: "ALHWARY BOT", jid: "ALHWARY BOT" },
         interactiveConfig: { buttons_limits: 10, list_title: "", button_title: "", canonical_url: url }
     }, m);
 };
